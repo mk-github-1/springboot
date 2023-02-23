240ba8317194481dc710304781b63c5f29d6c5f9
mk-github-1 <masaya.kamii@gmail.com>
2023-02-23 17:42:12 +0900

    修正

--- user-interface/src/main/java/com/example/userinterface/SpringDiConfig.java
+++ user-interface/src/main/java/com/example/userinterface/SpringDiConfig.java
@@ -1,20 +1,26 @@
 package com.example.userinterface;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+/*
 import com.example.applicationservice.service.implement.master.*;
 import com.example.applicationservice.service.master.*;
+import com.example.domainservice.repository.master.*;
+import com.example.infrastructure.implement.repository.master.*;
+ */
 
-// @Configuration
-// @ComponentScan("com.example.applicationservice.service.implement.master")
+@Configuration
 public class SpringDiConfig {
-
 	/*
 	@Bean
 	public PersonService personService(){
-		return new PersonServiceImplements();
+		return new PersonServiceImplements(new PersonRepositoryImplements());
+	}
+
+	@Bean
+	public PersonRepository personRepository(){
+		return new PersonRepositoryImplements();
 	}
 	 */
 }
