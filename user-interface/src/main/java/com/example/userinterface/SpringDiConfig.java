291413fb6d494f1a9a261ebcc841e9e7afb322be
mk-github-1 <masaya.kamii@gmail.com>
2023-02-24 05:33:46 +0900

    修正

--- user-interface/src/main/java/com/example/userinterface/SpringDiConfig.java
+++ user-interface/src/main/java/com/example/userinterface/SpringDiConfig.java
@@ -3,24 +3,24 @@ package com.example.userinterface;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-/*
 import com.example.applicationservice.service.implement.master.*;
 import com.example.applicationservice.service.master.*;
+
+/*
 import com.example.domainservice.repository.master.*;
 import com.example.infrastructure.implement.repository.master.*;
  */
 
+// 主にコンポーネントスキャンによる自動登録ができない（例. 外部ライブラリに含まれるクラスをBeanとして登録する）場合に使われる。
+// 自作クラスは@Service, @Repository, @ComponentアノテーションでOK
+
 @Configuration
 public class SpringDiConfig {
+
 	/*
 	@Bean
-	public PersonService personService(){
-		return new PersonServiceImplements(new PersonRepositoryImplements());
-	}
-
-	@Bean
-	public PersonRepository personRepository(){
-		return new PersonRepositoryImplements();
+	public PersonService personService() {
+		return new PersonService();
 	}
 	 */
 }
