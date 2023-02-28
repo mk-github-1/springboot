bef225734606b1c9671f437b2d29a3d802b1c854
mk-github-1 <masaya.kamii@gmail.com>
2023-03-01 05:34:51 +0900

    修正

--- user-interface/src/main/java/com/example/userinterface/WebSecurityConfig.java
+++ user-interface/src/main/java/com/example/userinterface/WebSecurityConfig.java
@@ -3,6 +3,8 @@ package com.example.userinterface;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -40,6 +42,13 @@ public class WebSecurityConfig {
 
         return http.build();
     }
+
+    /*
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+    	return authenticationConfiguration.getAuthenticationManager();
+    }
+     */
 
     @Bean
     public PasswordEncoder passwordEncoder() {
