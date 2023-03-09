49390a6de92fc925262aa889f6502f647171bc26
mk-github-1 <32920703+mk-github-1@users.noreply.github.com>
2023-03-10 08:18:53 +0900

    Update UserInterfaceApplication.java

--- user-interface/src/main/java/com/example/userinterface/UserInterfaceApplication.java
+++ user-interface/src/main/java/com/example/userinterface/UserInterfaceApplication.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Bean;
  * UserInterfaceApplication
  *
  */
+@ComponentScan("com.example.applicationservice", "com.example.infrastructure")
 @SpringBootApplication
 public class UserInterfaceApplication {
 
