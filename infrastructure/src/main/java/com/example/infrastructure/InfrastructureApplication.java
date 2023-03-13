9ae5be5045273827dbc1e3c955b1a67cad8df39f
mk-github-1 <masaya.kamii@gmail.com>
2023-03-13 23:18:16 +0900

    修正

--- infrastructure/src/main/java/com/example/infrastructure/InfrastructureApplication.java
+++ infrastructure/src/main/java/com/example/infrastructure/InfrastructureApplication.java
@@ -1,8 +1,12 @@
 package com.example.infrastructure;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class InfrastructureApplication {
 
 	public static void main(String[] args) {
-
+		SpringApplication.run(InfrastructureApplication.class, args);
 	}
 }
